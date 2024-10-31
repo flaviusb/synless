1495bea81a77ceb616d7f6c7df03ead37a4c5ba3
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2024-10-31 20:54:57 +1300

    More noodling.

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,26 @@ extern crate proc_macro;
 use proc_macro;
 
 // Result, Fail, CompileError, Accumulation
-trait<R, F, C, A> Ish<R, F, C, A> {
-  fn parse(self, it: IntoIter) -> ;
+// Accumulation is fn(R, Vec<Span>, R, Vec<Span>) -> (R, Vec<Span>)
+pub enum Ishy<R, F> {
+  Res(R, Vec<Span>),
+  Fail(F, Span),
+  CompileError(F, Span),
+}
+trait Ish<R, F> {
+  fn parse(self, it: IntoIter) -> Ishy<R, F>;
+}
+type Key = &str;
+pub enum Socket<Inner> {
+  DontCare,
+  Capture(Key),
+  Val(Inner),
 }
 pub enum TT<...> {
-  Item(),
-  Lit(),
-  Punct(),
-  Group(),
+  Item(Socket<Item>),
+  Lit(Socket<Lit>),
+  Punct(Socket<Punct>),
+  Group(Socket<Group>),
 }
 trait<..> TT<..> {
   fn item() ->
@@ -21,6 +33,18 @@ trait<..> TT<..> {
 trait<...> Ish<...> for TT<...> {
   fn parse(self, it: IntoIter) -> ;
 }
+pub struct Item;
+pub enum Lit;
+pub struct Punct {
+  pub spacing: Socket<Spacing>,
+  pub it: Socket<char>,
+}
+pub enum Spacing {
+  Joint,
+  Alone,
+}
+pub struct Group;
+
 pub struct Seq<T, ...> {
   sequence: Vec<T>,
   no_match: Option<fn ...>,
