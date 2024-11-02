73b45130bf380184fbb4d210cb5f7cb155693681
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2024-11-02 18:52:00 +1300

    More noodling.

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,10 @@ pub enum Socket<Inner> {
   Val(Inner),
 }
 pub enum TT<R, F> {
-  Item(Socket<Item>),
-  Lit(Socket<Lit>),
-  Punct(Socket<Punct>),
-  Group(Socket<Group>),
+  Item(Socket<Item<R, F>>),
+  Lit(Socket<Lit<R, F>>),
+  Punct(Socket<Punct<R, F>>),
+  Group(Socket<Group<R, F>>),
 }
 trait TT<R, F> {
   fn item() -> Self {
@@ -31,14 +31,20 @@ trait TT<R, F> {
   fn lit() -> Self {
     TT::<R, F>::Lit(Socket::Val(Lit {}))
   }
-  fn punct() ->
-  fn group() ->
+  fn punct() -> Self {
+    TT::<R, F>::Punct(Socket::DontCare)
+  }
+  fn group() -> Self {
+    TT::<R, F>::Group(Socket::Val(Group {}))
+  }
 }
-trait<R, F> Ish<R, F> for TT<R, F> {
-  fn parse(self, it: IntoIter) -> ;
+impl Ish<R, F> for TT<R, F> {
+  fn parse(self, it: IntoIter) -> Ishy<R, F> {
+    todo!()
+  }
 }
-pub struct Item;
-pub enum Lit;
+pub struct Item {}
+pub enum Lit {}
 pub struct Punct {
   pub spacing: Socket<Spacing>,
   pub it: Socket<char>,
@@ -47,16 +53,29 @@ pub enum Spacing {
   Joint,
   Alone,
 }
-pub struct Group;
+pub struct Group {}
 
-pub struct Seq<T, ...> {
-  sequence: Vec<T>,
-  no_match: Option<fn ...>,
+pub struct Seq<R, F> {
+  sequence: Vec<Box<Ish<R, F>>>,
+  no_match: Option<fn((R, Vec<Span>)) -> (F, Vec<Span>)>,
 }
-trait Ish for Seq... {
-  fn parse(self, it: IntoIter) -> ;
+impl Ish<R, F> for Seq<R, F> {
+  fn parse(self, it: IntoIter) -> Ishy<R, F> {
+    todo!()
+  }
 }
 
+pub struct Alt<R, F> {
+  sequence: Vec<Box<Ish<R, F>>>,
+  no_match: Option<fn((R, Vec<Span>)) -> (F, Vec<Span>)>,
+}
+impl Ish<R, F> for Alt<R, F> {
+  fn parse(self, it: IntoIter) -> Ishy<R, F> {
+    todo!()
+  }
+}
+
+/*
 Seq
 Alt
 Not
@@ -67,4 +86,4 @@ Maybe
 TT
 TTQ
 Map
-
+*/
